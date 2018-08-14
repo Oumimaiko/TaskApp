24bfc33a6696b00af0e032c69bf912f799163fda
Oumimaiko <makototokunaga0802@gmail.com>
2018-08-14 22:36:20 +0900

    5. Realmを使う

--- app/src/main/java/com/example/makoto/tokunaga/taskapp/TaskAdapter.java
+++ app/src/main/java/com/example/makoto/tokunaga/taskapp/TaskAdapter.java
@@ -7,17 +7,20 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-public class TaskAdapter extends BaseAdapter{
-    private List<String> mTaskList;
-    private LayoutInflater mLayoutInflater;
+public class TaskAdapter extends BaseAdapter {
+    private LayoutInflater mLayoutInflater = null;
+    private List<Task> mTaskList;
 
     public TaskAdapter(Context context) {
         mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    public void setTaskList(List<String> taskList) {
+    public void setTaskList(List<Task> taskList) {
         mTaskList = taskList;
     }
 
@@ -33,19 +36,24 @@ public class TaskAdapter extends BaseAdapter{
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return mTaskList.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView == null){
+
+        if (convertView == null) {
             convertView = mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null);
         }
 
         TextView textView1 = (TextView) convertView.findViewById(android.R.id.text1);
         TextView textView2 = (TextView) convertView.findViewById(android.R.id.text2);
 
-        textView1.setText(mTaskList.get(position));
+        textView1.setText(mTaskList.get(position).getTitle());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.JAPANESE);
+        Date date = mTaskList.get(position).getDate();
+        textView2.setText(simpleDateFormat.format(date));
 
         return convertView;
     }
